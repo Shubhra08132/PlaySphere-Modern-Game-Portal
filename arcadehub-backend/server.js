8af2bf6bba64a39c93b6f0f5ff7635509e701a56
Shubhra Singla <singlashubhra@gmail.com>
2025-09-06 14:24:41 +0530

    Updated backend

--- arcadehub-backend/server.js
+++ arcadehub-backend/server.js
@@ -21,12 +21,14 @@ app.use(express.json());
 app.use('/api', authRoutes); // Mounts all /login routes under /api
 
 // ✅ MongoDB connection
-mongoose.connect('mongodb://localhost:27017/arcade', {
+
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log(err));
+.then(() => console.log("✅ MongoDB Atlas Connected"))
+.catch(err => console.error("❌ MongoDB Connection Error:", err));
+
 
 // ✅ POST route to register user
 app.post('/api/register', async (req, res) => {
